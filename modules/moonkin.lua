90753753500138a811cc4a164551857dd3a19e40
Evl <wow.evl@gmail.com>
2010-01-07 10:14:17 +0100

    Better organization of bars and some naming clarifications.

--- modules/moonkin.lua
+++ modules/moonkin.lua
@@ -1,18 +1,4 @@
 if select(2, UnitClass("player")) == "DRUID" then
-	local ffDuration = 300
-	local insectswarmDuration = 14
-	local starfireGlyph = 54845
-	local moonfireDuration = function()
-		local maxValue = 15
-
-		-- Glyph of Starfire
-		if evl_SliceDice:hasGlyph(starfireGlyph) then 
-			maxValue = maxValue + 9
-		end
-
-		return maxValue
-	end	
-	
 	-- Eclipse
 	local eclipseDuration = 15
 	local eclipseCooldown = 15
@@ -47,15 +33,29 @@ if select(2, UnitClass("player")) == "DRUID" then
 	}
 
 	-- Insect Swarm
-	local isBar = evl_SliceDice:CreateBar("target", "Insect Swarm", insectswarmDuration, 14)
-	isBar.colors = {{0/255, 150/255, 0/255}}
+	local insectSwarmDuration = 14
+	local insectSwarmBar = evl_SliceDice:CreateBar("target", "Insect Swarm", insectSwarmDuration, 14)
+	insectSwarmBar.colors = {{0/255, 150/255, 0/255}}
 
 	-- Moonfire
-	local mfBar = evl_SliceDice:CreateBar("target", "Moonfire", moonfireDuration, 14)
-	mfBar.colors = {{150/255, 0/255, 255/255}}
+	local starfireGlyph = 54845
+	local moonfireDuration = function()
+		local maxValue = 15
+
+		-- Glyph of Starfire
+		if evl_SliceDice:hasGlyph(starfireGlyph) then 
+			maxValue = maxValue + 9
+		end
+
+		return maxValue
+	end	
+	
+	local moonfireBar = evl_SliceDice:CreateBar("target", "Moonfire", moonfireDuration, 14)
+	moonfireBar.colors = {{150/255, 0/255, 255/255}}
 
 	-- Faerie Fire
-	local ffBar = evl_SliceDice:CreateBar("target", "Faerie Fire", ffDuration, 6)
-	ffBar.auraFilter = "HARMFUL"
-	ffBar.label:Hide()
+	local faerieFireDuration = 300
+	local faerieFireBar = evl_SliceDice:CreateBar("target", "Faerie Fire", faerieFireDuration, 6)
+	faerieFireBar.auraFilter = "HARMFUL"
+	faerieFireBar.label:Hide()
 end
