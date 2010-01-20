e8731ae5009748f2956070ec783937aa6cd38cfd
Evl <wow.evl@gmail.com>
2010-01-20 19:55:20 +0100

    Adjusted bar heights.

--- modules/deathknight.lua
+++ modules/deathknight.lua
@@ -4,13 +4,13 @@ if select(2, UnitClass("player")) == "DEATHKNIGHT" then
 	
 	-- Blood Plague
 	local bloodPlagueDuration = 15
-	local bloodPlagueBar = evl_SliceDice:CreateBar("target", "Blood Plague", bloodPlagueDuration + diseaseDurationBonus, 9)
+	local bloodPlagueBar = evl_SliceDice:CreateBar("target", "Blood Plague", bloodPlagueDuration + diseaseDurationBonus, 6)
 	bloodPlagueBar.colors = {{200/255, 0/255, 0/255}}
 	bloodPlagueBar.label:Hide()
 
 	-- Frost Fever
 	local frostFeverDuration = 15 + diseaseDurationBonus
-	local frostFeverBar = evl_SliceDice:CreateBar("target", "Frost Fever", frostFeverDuration + diseaseDurationBonus, 9)
+	local frostFeverBar = evl_SliceDice:CreateBar("target", "Frost Fever", frostFeverDuration + diseaseDurationBonus, 6)
 	frostFeverBar.colors = {{0/255, 100/255, 150/255}}
 	frostFeverBar.label:Hide()
 end
