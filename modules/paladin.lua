095b8a91d2d328305766608c5afc90a9270974cd
Evl <wow.evl@gmail.com>
2010-10-18 08:39:31 +0200

    Added Art of War

--- modules/paladin.lua
+++ modules/paladin.lua
@@ -6,4 +6,10 @@ if addon.playerClass == "PALADIN" then
 	local sacredDutyBar = addon:CreateBar("player", 85433, sacredDutyDuration, 6)
 	sacredDutyBar.colors = {{200/255, 0/255, 0/255}}
 	sacredDutyBar.label:Hide()
+	
+	-- Art of War
+	local artOfWarDuration = 15
+	local artOfWarBar = addon:CreateBar("player", 59578, artOfWarDuration, 6)
+	artOfWarBar.colors = {{200/255, 0/255, 0/255}}
+	artOfWarBar.label:Hide()
 end
