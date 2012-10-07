68bb3c53f167f19698a47f695a8186d0392d6f16
Florian Anderiasch <fa@art-core.org>
2012-10-07 16:47:26 +0200

    MoP updates: Restore basic Druid functionality

--- modules/moonkin.lua
+++ modules/moonkin.lua
@@ -1,57 +1,9 @@
 local addonName, addon = ...
 
 if addon.playerClass == "DRUID" then
-	-- Eclipse
-	local eclipseDuration = 15
-	local eclipseCooldown = 15
-	local eclipseAuraFunction = function(bar, unit, spellName, rank, filter)
-		local name, _, _, _, _, _, expirationTime, _, _, _, spellId = UnitAura(unit, spellName, nil, filter)
-		
-		if name then
-			bar.eclipseExpiration = expirationTime + eclipseCooldown
-		end
-		
-		local timeLeft = (bar.eclipseExpiration or 0) - GetTime()
-		
-		if timeLeft > 0 then
-			return spellName, _, _, timeLeft <= eclipseCooldown and 1 or 2, _, _, bar.eclipseExpiration - (timeLeft > eclipseDuration and eclipseDuration or 0)
-		end
-		
-		return nil
-	end
-	
-	local solarEclipseBar = addon:CreateBar("player", 48517, eclipseDuration, 14)
-	solarEclipseBar.auraFunction = eclipseAuraFunction
-	solarEclipseBar.colors = {
-		{150/255, 100/255, 0/255},
-		{255/255, 150/255, 0/255},
-	}
-
-	local lunarEclipseBar = addon:CreateBar("player", 48518, eclipseDuration, 14)
-	lunarEclipseBar.auraFunction = eclipseAuraFunction
-	lunarEclipseBar.colors = {
-		{0/255, 100/255, 150/255},
-		{0/255, 150/255, 255/255},
-	}
-
-	-- Insect Swarm
-	local insectSwarmDuration = 14
-	local insectSwarmBar = addon:CreateBar("target", 5570, insectSwarmDuration, 14)
-	insectSwarmBar.colors = {{0/255, 150/255, 0/255}}
 
 	-- Moonfire
-	local starfireGlyph = 54845
-	local moonfireDuration = function()
-		local maxValue = 15
-
-		-- Glyph of Starfire
-		if addon:HasGlyph(starfireGlyph) then 
-			maxValue = maxValue + 9
-		end
-
-		return maxValue
-	end	
-	
+	local moonfireDuration = 14
 	local moonfireBar = addon:CreateBar("target", 8921, moonfireDuration, 14)
 	moonfireBar.colors = {{150/255, 0/255, 255/255}}
 
@@ -60,4 +12,14 @@ if addon.playerClass == "DRUID" then
 	local faerieFireBar = addon:CreateBar("target", 770, faerieFireDuration, 6)
 	faerieFireBar.auraFilter = "HARMFUL"
 	faerieFireBar.label:Hide()
+
+	-- Entangling Roots
+	local rootsDuration = 30
+	local rootsBar = addon:CreateBar("target", 339, rootsDuration, 6)
+	rootsBar.colors = {{0/255, 150/255, 0/255}}
+
+	-- Barkskin
+	local barkskinDuration = 14
+	local barkskinBar = addon:CreateBar("player", 22812, barkskinDuration, 3)
+	barkskinBar.colors = {{255/255, 150/255, 0/255}}
 end
